df2bd405be367195f25f5c51cc35ebeb19bb2e1d
barkode <konstantinks@gmail.com>
2024-07-11 23:03:44 +0100

    Optimize and convert to webp images

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -4,7 +4,7 @@ const defaultGameSettings = {
   cards: ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'],
   cardType: ['clubs', 'hearts', 'diamonds', 'spades'],
   deckName: 'deck1',
-  imgFormat: 'jpg',
+  imgFormat: 'webp',
 };
 
 const defaultAppSettings = {};
